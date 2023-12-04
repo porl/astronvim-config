76fbab915f621fcddf6bdf42a1e2eb3d851f82d7
porl <porl42@gmail.com>
2023-12-05 09:49:05 +1100

    added hexokinase (run make hexokinase in .local/share/nvim/lazy/nvim-hexokinase dir)

--- plugins/user.lua
+++ plugins/user.lua
@@ -22,5 +22,28 @@ return {
 				},
 			},
     },
-  }
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        -- this can be used if your configuration lives in ~/.config/nvim
+        -- if your configuration lives in ~/.config/astronvim, the full path
+        -- must be specified in the next line
+        paths = { "./lua/user/snippets" }
+      }
+    end,
+  },
+  {
+    "rrethy/vim-hexokinase",
+    lazy = false,
+    config = function()
+        --        vim.cmd [[ make hexokinase ]]
+        vim.g.Hexokinase_optInPatterns = "full_hex,triple_hex,rgb,rgba,hsl,hsla"
+        vim.cmd [[ HexokinaseTurnOn ]]
+    end,
+  },
 }
